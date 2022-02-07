6d483273b627eeb7b623001df8b209823cfb4024
Connie Bernardin <conniebernardin@googlemail.com>
2022-02-07 17:15:55 +0000

    adding players and teams

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,23 +7,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Player Fin = new Player("Fin Russell", "Fly Half", 10, 12);
-        Manager manager = new Manager("Gregor Townsend");
-        Team scotland =  new Team("Scotland", manager);
+        Player Rachel = new Player("Rachel Cook", "Blindside Flanker", 10, 12);
+        Player Abi = new Player("Abi Evans", "Left Wing", 12, 24);
+        Player Sarah = new Player("Sarah Law", "Scrum Half", 02, 4);
+        Player Lisa = new Player("Lisa Martin", "Inside Centre", 13, 12);
+        Player Heather = new Player("Heather Lockheart", "Loosehead Prop", 05, 8);
+
+        Player SarahB = new Player("Sarah Bern", "Prop", 06, 12);
+        Player Hannah = new Player("Hannah Botterman", "Prop", 02, 24);
+        Player Vicky = new Player("Vicky Fleetwood", "Flanker", 13, 16);
+        Player Connie = new Player("Connie Powell", "Hooker", 01, 10);
+
+        Manager Caroline = new Manager("Caroline Sport");
+        Manager Sophie = new Manager("Sophie Rugby");
+        Team scotland =  new Team("Scotland", Caroline);
+        Team england =  new Team("England", Sophie);
 
         //calling count empty spaces method on Scotland team
         TeamService teamService = new TeamService();
-        int spacesBefore = teamService.countEmptySpacesInTeam(scotland);
-        System.out.println(spacesBefore);
-
+        System.out.println("Scotland Team Capacity:");
+        System.out.println("------------------------");
+        int spacesBeforeScotland = teamService.countEmptySpacesInTeam(scotland);
+        System.out.println(spacesBeforeScotland);
 
         try {
-            teamService.addPlayerToTeam(scotland, Fin);
+            teamService.addPlayerToTeam(scotland, Rachel);
+            teamService.addPlayerToTeam(scotland, Abi);
+            teamService.addPlayerToTeam(scotland, Sarah);
+            teamService.addPlayerToTeam(scotland, Lisa);
+            teamService.addPlayerToTeam(scotland, Heather);
+            teamService.addPlayerToTeam(england, SarahB);
+            teamService.addPlayerToTeam(england, Hannah);
+            teamService.addPlayerToTeam(england, Vicky);
+            teamService.addPlayerToTeam(england, Connie);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        int spacesAfter = teamService.countEmptySpacesInTeam(scotland);
-        System.out.println(spacesAfter);
+        int spacesAfterScotland = teamService.countEmptySpacesInTeam(scotland);
+        System.out.println(spacesAfterScotland);
+        System.out.println("Who plays for Scotland?");
+        System.out.println(scotland);
+
+        System.out.println("Who plays for England?");
+        System.out.println(england);
     }
 }
