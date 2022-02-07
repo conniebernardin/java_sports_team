323a000c13ba524b960aa6459f9e58e0e04d9d00
Connie Bernardin <conniebernardin@googlemail.com>
2022-02-07 14:03:18 +0000

    count empty spaces in team method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import POJOs.Manager;
 import POJOs.Player;
 import POJOs.Team;
+import Services.TeamService;
 
 public class Main {
 
@@ -9,5 +10,9 @@ public class Main {
         Player player = new Player("Fin Russell", "Fly Half", 10, 12);
         Manager manager = new Manager("Gregor Townsend");
         Team scotland =  new Team("Scotland", manager);
+
+        TeamService teamService = new TeamService();
+        int spaces = teamService.countEmptySpacesInTeam(scotland);
+        System.out.println(spaces);
     }
 }
--- src/main/java/Services/TeamService.java
+++ src/main/java/Services/TeamService.java
@@ -1,5 +1,28 @@
 package Services;
 
+import POJOs.Player;
+import POJOs.Team;
+
 public class TeamService {
-    
+
+    public int countEmptySpacesInTeam(Team team) {
+        // temporary variable to store counts
+        int counter = 0;
+        //loop through player array to check for nulls
+        for (Player player : team.getPlayers()) { //for each player object in players Array (accessed through team class)
+            //if statement
+            if (player == null){
+                counter ++;
+
+                /* could also use:
+                 count += 1;
+                 count = count + 1; */
+            }
+        }
+        return counter;
+    }
 }
+//method to count empty spaces on team
+//return int
+// parameters: Team
+
